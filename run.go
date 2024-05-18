1da36055064137bf9a6e71e79a16497770b6eb3a
TL <tanglong8@126.com>
2024-05-19 01:29:35 +0800

    docker -d

--- run.go
+++ run.go
@@ -35,8 +35,9 @@ func Run(tty bool, comArray []string, res *subsystems.ResourceConfig, volume str
 
 	// 在子进程创建后才能通过pipe来发送参数
 	sendInitCommand(comArray, writePipe)
+
 	_ = parent.Wait()
-	//container.DeleteWorkSpace("/root/", volume)
+
 }
 
 // sendInitCommand 通过writePipe将指令发送给子进程
